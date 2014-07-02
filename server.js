b6e09cf4ef4120cad285c0e06d3a415f01074df0
kiwiyuki <kiwi.kyuui@gmail.com>
2014-07-02 16:18:14 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var server = require('http').createServer(httpHandler);
+var	io = require('socket.io').listen(server);
+var fs= require('fs');
+server.listen(80);
+
+function httpHandler (req, res) {
+	fs.readFile(__dirname + '/index.html', function  (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error');
+		}
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+	});
+}
+var players = [];
+var counter = 0;
+
+io.sockets.on('connection', function (socket){
+	//各プレイヤー情報受信
+	var p = {
+		"id" : socket.id,
+		"x"	: 0,
+		"y"	: 0
+	};
+	players.push(p);
+	socket.on('data_change', function (data) {
+		for(var i = 0; i < players.length; i++) {
+			var pyr = players[i];
+			if (pyr["id"] == socket.id) {
+				pyr["x"] = data.x;
+				pyr["y"] = data.y;
+				break;
+			}
+		}
+		io.sockets.emit("data_players",players)
+		socket.broadcast.json.emit(players);
+	});
+});
